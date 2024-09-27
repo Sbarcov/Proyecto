446e4b6995a5af426cded6023b53682f1866e69c
Sebastian B <sebastian.barcovsci@hotmail.com>
2024-09-27 19:03:31 -0300

    creado componente card y creado archivo cardData

---
+++ components/card.js
@@ -0,0 +1,17 @@
+const url = 'http://127.0.0.1:5500/'
+
+export const cardComponent = (img, title, desc, price) => {
+    return `
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+          <img src="${img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${desc}</p>
+                <p class="card-text">Precio: $${price}</p>
+                <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+            </div>
+        </div>
+    </div>
+    `
+}
---
+++ data/cardData.js
@@ -0,0 +1,53 @@
+export const joyeriaData = [
+    {   img: 'img/ring.webp',
+        title: 'Pieza de joyeria 1',
+        desc: 'Descripción para joya 1',
+        price: 100 },
+
+    {   img: 'img/ring.webp',
+        title: 'Pieza de joyeria 2',
+        desc: 'Descripción para joya 2',
+        price: 200 },    
+
+    {   img: 'img/ring.webp',
+        title: 'Pieza de joyeria 3',
+        desc: 'Descripción para joya 3',
+        price: 300 },
+
+]
+
+export const prendasData = [
+    {   img: 'img/clothes.webp',
+        title: 'Prenda 1',
+        desc: 'Descripción para renda 1',
+        price: 150 },
+
+    {   img: 'img/clothes.webp',
+        title: 'Prenda 2',
+        desc: 'Descripción para prenda 2',
+        price: 250 },    
+
+    {   img: 'img/clothes.webp',
+        title: 'Prenda 3',
+        desc: 'Descripción para prenda 3',
+        price: 350 },
+
+]
+
+export const electronicaData = [
+    {   img: 'img/tech.webp',
+        title: 'Producto 1',
+        desc: 'Descripción para Producto 1',
+        price: 175 },
+
+    {   img: 'img/tech.webp',
+        title: 'Producto 2',
+        desc: 'Descripción para Producto 2',
+        price: 275 },    
+
+    {   img: 'img/tech.webp',
+        title: 'Producto 3',
+        desc: 'Descripción para Producto 3',
+        price: 375 },
+
+]
