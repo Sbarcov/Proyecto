b5adee0364bd45ccfdf245125ff8358d7e521e87
Sebastian B <sebastian.barcovsci@hotmail.com>
2024-09-29 17:10:32 -0300

    datos unificados en una sola estructura

--- data/cardData.js
+++ data/cardData.js
@@ -1,53 +1,56 @@
-export const joyeriaData = [
+export const data = [
     {   img: 'img/ring.webp',
         title: 'Pieza de joyeria 1',
         desc: 'Descripción para joya 1',
-        price: 100 },
+        price: 100,
+        category: 'joyeria' },
 
     {   img: 'img/ring.webp',
         title: 'Pieza de joyeria 2',
         desc: 'Descripción para joya 2',
-        price: 200 },    
+        price: 200,
+        category: 'joyeria' },    
 
     {   img: 'img/ring.webp',
         title: 'Pieza de joyeria 3',
         desc: 'Descripción para joya 3',
-        price: 300 },
+        price: 300,
+        category: 'joyeria' },
 
-]
-
-export const prendasData = [
     {   img: 'img/clothes.webp',
         title: 'Prenda 1',
         desc: 'Descripción para renda 1',
-        price: 150 },
+        price: 150,
+        category: 'prendas' },
 
     {   img: 'img/clothes.webp',
         title: 'Prenda 2',
         desc: 'Descripción para prenda 2',
-        price: 250 },    
+        price: 250,
+        category: 'prendas' },    
 
     {   img: 'img/clothes.webp',
         title: 'Prenda 3',
         desc: 'Descripción para prenda 3',
-        price: 350 },
+        price: 350,
+        category: 'prendas' },
 
-]
-
-export const electronicaData = [
     {   img: 'img/tech.webp',
         title: 'Producto 1',
         desc: 'Descripción para Producto 1',
-        price: 175 },
+        price: 175,
+        category: 'electronica' },
 
     {   img: 'img/tech.webp',
         title: 'Producto 2',
         desc: 'Descripción para Producto 2',
-        price: 275 },    
+        price: 275,
+        category: 'electronica' },    
 
     {   img: 'img/tech.webp',
         title: 'Producto 3',
         desc: 'Descripción para Producto 3',
-        price: 375 },
+        price: 375,
+        category: 'electronica' },
 
 ]
