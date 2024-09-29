5e68d047a4f216ce65cd14c8e52328ed9a150a09
Sebastian B <sebastian.barcovsci@hotmail.com>
2024-09-29 17:11:20 -0300

    script de carga para cards unificado

--- js/cardLoad.js
+++ js/cardLoad.js
@@ -1,19 +1,26 @@
 import { navbarComp } from "../components/navbar.js";
 import { cardComponent } from "../components/card.js";
-import { joyeriaData} from "../data/cardData.js";
+import { data} from "../data/cardData.js";
 
 let navContainer = document.querySelector('header')
 let cardContainer = document.getElementById('cardElement')
+let containerId = document.getElementsByClassName('container-sm')[0].id
 
 window.addEventListener('load', ()=>{
     navContainer.innerHTML = navbarComp
 })
 
 window.addEventListener('load', () =>{
-    const cards = joyeriaData.map(e =>{
+
+    const category = data.filter(e => {
+        return e.category == containerId;
+    });
+
+    const cards = category.map(e =>{
         return cardContainer.innerHTML = cardComponent(e.img, e.title, e.desc, e.price)
     }).join('')
 
     cardContainer.innerHTML = cards
+
 
 })
