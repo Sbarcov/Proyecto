4c5112e567870aa11386109230147cfba59af4e3
Sebastian B <sebastian.barcovsci@hotmail.com>
2024-10-30 15:03:07 -0300

    Fetch

--- components/card.js
+++ components/card.js
@@ -2,7 +2,7 @@ export const cardComponent = (img, title, desc, price) => {
     return `
     <div class="col">
         <div class="card" style="width: 18rem;">
-          <img src="${img}" class="card-img-top" alt="...">
+          <img src="${img}" class="tarjeta" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${title}</h5>
                 <p class="card-text">${desc}</p>
--- js/cardLoad.js
+++ js/cardLoad.js
@@ -1,27 +1,58 @@
 import { navbarComp } from "../components/navbar.js";
 import { cardComponent } from "../components/card.js";
-import { data} from "../data/cardData.js";
+import { data } from "../data/cardData.js";
 
 let navContainer = document.querySelector('header') // Se anexo el navbar a este script
 
 let cardContainer = document.getElementById('cardElement')
 let containerId = document.getElementsByClassName('container-sm')[0].id
 
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
     navContainer.innerHTML = navbarComp
 })
 
-window.addEventListener('load', () =>{
+/*
+window.addEventListener('load', () => {
 
     const category = data.filter(e => {
         return e.category == containerId;
     });
 
-    const cards = category.map(e =>{
+    const cards = category.map(e => {
         return cardContainer.innerHTML = cardComponent(e.img, e.title, e.desc, e.price)
     }).join('')
 
     cardContainer.innerHTML = cards
 
+})
+    */
+
+window.addEventListener('load', () => {
+
+   fetch('https://fakestoreapi.com/products').then(res => res.json()).then(json =>{
+
+        let filteredItems = []
+
+        if(containerId == 'destacados') {
+
+            for (var i = 0; i < 6; i++) {
+                filteredItems[i] = json[i*2]
+              }
+
+        }
+        else {
+
+            filteredItems = json.filter(e => e.category == containerId);
+            
+        }
+
+        const cards = filteredItems.map(e => {
+            return cardContainer.innerHTML = cardComponent(e.image, e.title, e.description, e.price)
+        }).join('')
+
+        cardContainer.innerHTML = cards
+
+   }) 
 
 })
+
