59df2052ba59a686ebb39c04642ff26f1d9dd389
Sebastian B <sebastian.barcovsci@hotmail.com>
2024-11-01 16:46:12 -0300

    mod

--- js/cardLoad.js
+++ js/cardLoad.js
@@ -1,6 +1,5 @@
 import { navbarComp } from "../components/navbar.js";
 import { cardComponent } from "../components/card.js";
-import { data } from "../data/cardData.js";
 
 let navContainer = document.querySelector('header') // Se anexo el navbar a este script
 
@@ -11,65 +10,42 @@ const getUserData = (key) => {
     return JSON.parse(sessionStorage.getItem(key));
 }
 
+
 window.addEventListener('load', ()=>{
 
     const userInfo = getUserData('userData');
 
     if (userInfo){
+
         navContainer.innerHTML = navbarComp;
+        
+        fetch('https://fakestoreapi.com/products').then(res => res.json()).then(json =>{
+
+            let filteredItems = []
+        
+            if(containerId == 'destacados') {
+        
+                for (var i = 0; i < 6; i++) {
+                    filteredItems[i] = json[i*2]
+                  }
+        
+            }
+            else {
+        
+                filteredItems = json.filter(e => e.category == containerId);
+                
+            }
+        
+            const cards = filteredItems.map(e => {
+                return cardContainer.innerHTML = cardComponent(e.image, e.title, e.description, e.price)
+            }).join('')
+        
+            cardContainer.innerHTML = cards
+        
+        }) 
     }
     else
     {
         window.location.href = './login.html';
     }
 })
-
-window.addEventListener('load', () => {
-    navContainer.innerHTML = navbarComp
-})
-
-/*
-window.addEventListener('load', () => {
-
-    const category = data.filter(e => {
-        return e.category == containerId;
-    });
-
-    const cards = category.map(e => {
-        return cardContainer.innerHTML = cardComponent(e.img, e.title, e.desc, e.price)
-    }).join('')
-
-    cardContainer.innerHTML = cards
-
-})
-    */
-
-window.addEventListener('load', () => {
-
-   fetch('https://fakestoreapi.com/products').then(res => res.json()).then(json =>{
-
-        let filteredItems = []
-
-        if(containerId == 'destacados') {
-
-            for (var i = 0; i < 6; i++) {
-                filteredItems[i] = json[i*2]
-              }
-
-        }
-        else {
-
-            filteredItems = json.filter(e => e.category == containerId);
-            
-        }
-
-        const cards = filteredItems.map(e => {
-            return cardContainer.innerHTML = cardComponent(e.image, e.title, e.description, e.price)
-        }).join('')
-
-        cardContainer.innerHTML = cards
-
-   }) 
-
-})
-
